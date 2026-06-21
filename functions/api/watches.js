198186634c98c409e677adbc44e273307b3a1626
staplefordwatches <ben@staplefordwatches.co.uk>
2026-06-21 08:24:41 +0200

    Update watches.js

--- functions/api/watches.js
+++ functions/api/watches.js
@@ -61,7 +61,7 @@ export default async function handler(req, res) {
       };
     });
 
-    res.status(200).json({ watches });
+    res.status(200).json(watches);
   } catch (e) {
     res.status(500).json({ error: "Could not load watches", detail: e.message });
   }
